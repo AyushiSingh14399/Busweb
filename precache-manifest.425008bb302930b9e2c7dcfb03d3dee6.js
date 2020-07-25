5e57c53416d96db2a5386cbabe7dd5a905ccdb37
AyushiSingh14399 <53054160+AyushiSingh14399@users.noreply.github.com>
2020-07-25 23:48:12 +0530

    Updates

---
+++ precache-manifest.425008bb302930b9e2c7dcfb03d3dee6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8348581f092eee85b8fb12a14efcfd8d",
+    "url": "/busweb/index.html"
+  },
+  {
+    "revision": "a56ab7be58925171d4ed",
+    "url": "/busweb/static/css/main.47a774ef.chunk.css"
+  },
+  {
+    "revision": "b1f0dcf1897780ec2ee5",
+    "url": "/busweb/static/js/2.0af53e89.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/busweb/static/js/2.0af53e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56ab7be58925171d4ed",
+    "url": "/busweb/static/js/main.4b82795d.chunk.js"
+  },
+  {
+    "revision": "432f6b0154efbf758589",
+    "url": "/busweb/static/js/runtime-main.486ce83f.js"
+  },
+  {
+    "revision": "d23576818c12c643ef6d91dc473b264a",
+    "url": "/busweb/static/media/Intel.d2357681.png"
+  },
+  {
+    "revision": "cd724000532bd551f31d8652b2d2c0d3",
+    "url": "/busweb/static/media/back5.cd724000.jpg"
+  },
+  {
+    "revision": "5ae7767a633228545b9d6386c8dfb6c5",
+    "url": "/busweb/static/media/banner.5ae7767a.jpg"
+  },
+  {
+    "revision": "3f2306524345590a04062a5458414dc4",
+    "url": "/busweb/static/media/contact.3f230652.php"
+  },
+  {
+    "revision": "aab222226dd5589b204d7d6b20c0a036",
+    "url": "/busweb/static/media/custom.aab22222.jpg"
+  },
+  {
+    "revision": "8cc17cf722cf7783d0e69506cbe01a1b",
+    "url": "/busweb/static/media/dell.8cc17cf7.jpg"
+  },
+  {
+    "revision": "7dd5d9d5b91fbc210508c0a97f996ae2",
+    "url": "/busweb/static/media/hp.7dd5d9d5.png"
+  },
+  {
+    "revision": "3d519ce2a267a140f9c962038510b7c8",
+    "url": "/busweb/static/media/laptop2.3d519ce2.jpg"
+  },
+  {
+    "revision": "efaec5b059753cfab836b18b08e7939c",
+    "url": "/busweb/static/media/repair.efaec5b0.jpg"
+  },
+  {
+    "revision": "67d2d5b0f529ddcb5b0f1505a5b95589",
+    "url": "/busweb/static/media/tally.67d2d5b0.png"
+  }
+]);
